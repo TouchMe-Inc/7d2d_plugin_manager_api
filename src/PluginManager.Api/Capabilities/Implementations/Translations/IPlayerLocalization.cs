e849f35d536b693fa8b96cf9118e3ab7c96a178d
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2026-04-24 22:07:56 +0300

    Update for PluginManger.Localization

--- src/PluginManager.Api/Capabilities/Implementations/Translations/IPlayerLocalization.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PluginManager.Api.Capabilities.Implementations.Translations;
-
-public interface IPlayerLocalization
-{
-    string Translate(string platformId, string key, params object[] args);
-    string Translate(string platformId, string key);
-}
--- src/PluginManager.Api/Capabilities/Implementations/Translations/IPlayerLocalizationFactory.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace PluginManager.Api.Capabilities.Implementations.Translations;
-
-public interface IPlayerLocalizationFactory : ICapability
-{
-    IPlayerLocalization Create(string langPath);
-}
