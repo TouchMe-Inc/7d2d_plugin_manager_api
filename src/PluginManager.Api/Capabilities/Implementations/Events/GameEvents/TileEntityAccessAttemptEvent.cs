9a5f994ce96826f67675ec3702de611fd01f05c6
Dmitry Pavlukov <89782512+TouchMe-Inc@users.noreply.github.com>
2026-04-29 01:17:58 +0300

    Add TileEntityId for TileEntityAccessAttemptEvent

--- src/PluginManager.Api/Capabilities/Implementations/Events/GameEvents/TileEntityAccessAttemptEvent.cs
+++ src/PluginManager.Api/Capabilities/Implementations/Events/GameEvents/TileEntityAccessAttemptEvent.cs
@@ -5,6 +5,7 @@ namespace PluginManager.Api.Capabilities.Implementations.Events.GameEvents;
 
 public class TileEntityAccessAttemptEvent(
     int entityId,
+    int tileEntityId,
     TileEntityType tileEntityType,
     Vector3Int tileEntityPosition
 ) : ProxyObject, IGameEvent
@@ -12,6 +13,7 @@ public class TileEntityAccessAttemptEvent(
     public string EventName => nameof(TileEntityAccessAttemptEvent);
 
     public int EntityId { get; set; } = entityId;
+    public int TileEntityId { get; set; } = tileEntityId;
     public TileEntityType TileEntityType { get; set; } = tileEntityType;
     public Vector3Int TileEntityPosition { get; set; } = tileEntityPosition;
 }
